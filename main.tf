a190267a68bbae497c2c2ec1f52eab7035c8db11
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-11-06 02:57:20 -0200

    Terraform config to launch packer custom ami image

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "${var.region}"
+}
+
+resource "aws_instance" "packer" {
+  ami = "ami-5df2d34a"
+  instance_type = "t2.micro"
+}
+  
